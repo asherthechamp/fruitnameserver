243d10a30880da0159b4ce31c5ae2ceaf6430966
Ashenafi <ashukite@gmail.com>
2024-06-19 11:04:20 +0300

    seventh commit

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const serverless = require("serverless-http");
 
 const app = express() 
 
-const port = 5000
+const router = express.Router()
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/data.json")) 
 })
 
-app.listen(port, console.log("Server listening to port 5000 ..."))
-
+app.use(router)
 module.exports = {
     handler : serverless(app)
 } 
